aec7ab77ab005d1b38fdbcb376675db82bc69bcb
Tim Schwieger <tim.schwieger20@gmail.com>
2021-06-03 15:05:32 +0200

    let

--- src/javascript/themes.js
+++ src/javascript/themes.js
@@ -1,15 +1,15 @@
 /*Dark-Mode*/
 function darkMode() {
     let retrievedObject = JSON.parse(localStorage.getItem("darkmode"));
-
+    let theme;
 
 
     if (retrievedObject.src == "theme-dark") {
         document.documentElement.className = "theme-light";
-        let theme = { type: "darkmode", src: "theme-light" };
+        theme = { type: "darkmode", src: "theme-light" };
     } else {
         document.documentElement.className = "theme-dark";
-        let theme = { type: "darkmode", src: "theme-dark" };
+        theme = { type: "darkmode", src: "theme-dark" };
     }
 
     localStorage.setItem("darkmode", JSON.stringify(theme));
